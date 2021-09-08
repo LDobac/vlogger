c741c6e553f5ecae5da51a8b0391aebeda97575c
yikolden <ljh34210329@gmail.com>
2021-09-08 19:29:16 +0900

    Design 404 and error page simply.

--- src/views/errors/404.vue
+++ src/views/errors/404.vue
@@ -1,11 +1,28 @@
 <template>
-    <div class="card page-not-found">
-        404 Not Found
+    <div class="page-not-found">
+        <p>404</p>
+        <p>Page Not Found</p>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .page-not-found {
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 2;
+    grid-row-end: 4;
+
+    display: flex;
+    background: white;
     width: 100%;
+    height: 600px;
+    margin-top: 100px;
+    text-align: center;
+
+    flex-direction: column;
+    justify-content: center;
+
+    font-size: 2.5rem;
+    font-weight: bold;
 }
 </style>
--- src/views/errors/500.vue
+++ src/views/errors/500.vue
@@ -1,5 +1,27 @@
 <template>
     <div class="error">
-        Error
+        <p>Error!</p>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.error {
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 2;
+    grid-row-end: 4;
+
+    display: flex;
+    background: white;
+    width: 100%;
+    height: 600px;
+    margin-top: 100px;
+    text-align: center;
+
+    flex-direction: column;
+    justify-content: center;
+
+    font-size: 2.5rem;
+    font-weight: bold;
+}
+</style>
