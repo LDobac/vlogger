c63cd0efa968ab32b8af8277f8b82638b5a12e0b
yikolden <ljh34210329@gmail.com>
2021-10-25 01:50:43 +0900

    게시글 높임말으로 일치

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import { trackRouter } from "vue-gtag-next";
 
 import Home from "../views/Home.vue";
@@ -35,7 +35,6 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-    // history: createWebHistory(process.env.BASE_URL),
     history: createWebHashHistory(process.env.BASE_URL),
     routes,
     scrollBehavior(to, from, savedPosition) {
